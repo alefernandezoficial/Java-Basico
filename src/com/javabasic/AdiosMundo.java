221bdae7fddc151e12f838aace0ffa2b4369634b
alefernandezoficial <alefernandezoficial@gmail.com>
2023-01-23 23:13:31 -0300

    Updated

--- src/com/javabasic/AdiosMundo.java
+++ src/com/javabasic/AdiosMundo.java
@@ -1,10 +1,29 @@
 package com.javabasic;
 
+/*
+ * Clase de prueba para crear un programa basico en Java
+ */
+
 public class AdiosMundo {
     
+/**
+ * Punto de entrada a la aplicacion java
+ * permite ejecutar la aplicacion
+ * @param args - argumentos de entrada para la aplicacion
+ */
+
     public static void main(String[] args) {
         
-        System.out.println("Adios Mundo");
-        
+        // comentario1
+        System.out.println("Adios Mundo"); // comentario2
+
+        /*
+         *  comentario multilinea:
+         *  1- Crear funcion suma
+         *  2- Crear variables tipo numerico
+         *  3- Utilizar la funcion suma para calcular la suma de dos numeros
+         * 
+         */
+
     }
 }
---
+++ src/com/javabasic/Tipos.java
@@ -0,0 +1,54 @@
+package com.javabasic;
+
+/**
+ * Crear un proyecto java desde cero.
+ * Crear un paquete.
+ * Dentro del paquete crear una clase.
+ * Dentro de la clase crear un metodo main.
+ * Imprimir todos los tipos de datos vistos.
+ */
+
+public class Tipos {
+    public static void main(String[] args) {
+        
+        // 1. Numeros
+
+        // 1.1 Enteros
+        byte variable1 = 5;
+        short variable2 = 10;
+        int variable3 = 30;
+        long variable4 = 100;
+
+        // 1.2 Decimales
+        float variable5 = 5.5f;
+        double variable6 = 10.5d;
+
+        // 2. Booleano
+        boolean variable7 = false;
+        boolean variable8 = true;
+
+        // 3. Texto
+        char variable9 = 'a';
+        String variable10 = "Texto";
+
+
+        // Impresion de todos los tipos de datos
+        // 1.1 Enteros
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+
+        // 1.2 Decimales
+        System.out.println(variable5);
+        System.out.println(variable6);
+
+        // 2. Booleano
+        System.out.println(variable7);
+        System.out.println(variable8);
+
+        // 3. Texto
+        System.out.println(variable9);
+        System.out.println(variable10);
+    }
+}
