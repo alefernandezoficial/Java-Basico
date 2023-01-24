4c6eb1b69d0fd7689fdcbad68dcb69a8f6008abe
alefernandezoficial <alefernandezoficial@gmail.com>
2023-01-24 16:53:16 -0300

    Updated

--- src/com/example/ejercicio3.java
+++ src/com/example/ejercicio3.java
@@ -4,7 +4,13 @@ public class ejercicio3 {
     
     public static void main(String[] args) {
         
-        
+        // Array de tipo String (Texto)
+        String[] texto = new String[]{"Alejandro", "Alexis", "Roberta", "Lola"};
+
+        // Recorre array y se muestra el contenido
+        for(int i = 0; i < texto.length; i++){
+            System.out.println(texto[i]);
+        }
 
     }
 }
