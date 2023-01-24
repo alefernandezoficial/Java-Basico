eef33b238f969068f93da70624c2e64b2e9aefb4
alefernandezoficial <alefernandezoficial@gmail.com>
2023-01-24 19:00:43 -0300

    Updated

---
+++ src/com/example/ejerciciotema4/Main.java
@@ -0,0 +1,13 @@
+package com.example.ejerciciotema4;
+
+public class Main{
+public static void main(String[] args) {
+
+    SmartPhone smartPhone = new SmartPhone("Samsung", "S10", "Azul", 8, "Android");
+
+    SmartWatch smartWatch = new SmartWatch("Xiaomi", "Mi Band 6", "Negro", 3.5, "Bluetooth");
+
+    System.out.println("El smartphone es: " + smartPhone + "\nEl smartwatch es: " + smartWatch);
+
+    }
+}
---
+++ src/com/example/ejerciciotema4/SmartDevice.java
@@ -0,0 +1,17 @@
+package com.example.ejerciciotema4;
+
+public class SmartDevice{
+
+        String marca;
+        String modelo;
+        String color;
+
+        public SmartDevice() {
+        }
+
+        public SmartDevice(String marca, String modelo, String color) {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.color = color;
+        }
+}
