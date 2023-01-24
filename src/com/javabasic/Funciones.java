68ffc09e5cdd493594ed40afd5f90966821d3de3
alefernandezoficial <alefernandezoficial@gmail.com>
2023-01-24 01:43:38 -0300

    Updated

--- src/com/javabasic/Funciones.java
+++ src/com/javabasic/Funciones.java
@@ -3,24 +3,40 @@ package com.javabasic;
 public class Funciones {
     public static void main(String[] args) {
 
-        // Opcion 1: funcion sin parametros y SIN tipo de retorno
+        // Opcion 1: funcion sin parametros y SIN tipo de retorno.
         //showMenu();
         //showMenu();
 
-
-        // Opcion2: funcion sin parametros y CON tipo de retorno
+        // Opcion 2: funcion sin parametros y CON tipo de retorno.
         String menu = getMenu();
         System.out.println(menu); // getMenu();
 
         double coins = getCOINS();
         System.out.println(coins);
 
+        // Opcion 3: funcion con parametros y SIN tipo de retorno.
+        imprimirSaludoBuenosDias("EntryFragger");
+
+        // Opcion 4: funcion con parametros y CON tipo de retorno.
+        String apellido = "Fernandez";
+        String nombre = "Alejandro";
+        String saludo = obtenerSaludo(nombre, apellido);
+        System.out.println(saludo);
+
+    }
+
+    static String obtenerSaludo(String nombre, String apellido){
+        return "Wellcome " + nombre + " , " + apellido;
+
+    }
+
+    static void imprimirSaludoBuenosDias(String roll){
+        System.out.println("Jugador " + roll);
     }
 
     static double getCOINS() {
         return 99.23;
     }
-
 
 /**
  *      void indica que no devuelve ningun dato
