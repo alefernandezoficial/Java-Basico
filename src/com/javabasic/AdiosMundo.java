cb2eda247e3e1b5282120e024d0c41144cd2376f
alefernandezoficial <alefernandezoficial@gmail.com>
2023-01-23 22:36:20 -0300

    Updated

---
+++ src/com/javabasic/AdiosMundo.java
@@ -0,0 +1,10 @@
+package com.javabasic;
+
+public class AdiosMundo {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Adios Mundo");
+        
+    }
+}
---
+++ src/com/javabasic/HolaMundo.java
@@ -0,0 +1,10 @@
+package com.javabasic;
+
+public class HolaMundo {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hola Mundo");
+        
+    }
+}
