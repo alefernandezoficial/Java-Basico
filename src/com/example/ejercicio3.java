d109a513777847f837711dc41e24aea2d869e41c
alefernandezoficial <alefernandezoficial@gmail.com>
2023-01-24 16:50:58 -0300

    Updated

---
+++ src/com/example/ejercicio3.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class ejercicio3 {
+    
+    public static void main(String[] args) {
+        
+        
+
+    }
+}
---
+++ src/com/javabasic/estructurascontrol/repeticion/ConcatenarTextos.java
@@ -0,0 +1,14 @@
+package com.javabasic.estructurascontrol.repeticion;
+
+public class ConcatenarTextos {
+    
+    public static void main(String[] args) {
+        
+        String[] nombres = {"Alejandro", "Alexis", "Roberta", "Lola"};
+
+        for(String nombre : nombres){
+            System.out.println(nombre);
+        }
+
+    }
+}
