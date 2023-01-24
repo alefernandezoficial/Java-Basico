e5704e6ccd92c56e051b938d06196af9a01ae924
alefernandezoficial <alefernandezoficial@gmail.com>
2023-01-23 23:36:25 -0300

    Updated

---
+++ src/com/example/ejercicio1.java
@@ -0,0 +1,43 @@
+package com.example;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+        
+        // Enteros
+        byte number1 = 1;
+        short number2 = 10;
+        int number3 = 50;
+        long number4 = 100;
+
+        // Impresion de numeros enteros
+        System.out.println("Numeros enteros: " + number1 + " , " + number2 + " , " + number3 + " , " + number4);
+
+
+
+        // Decimales
+        float decimal1 = 2.9f;
+        double decimal2 = 8.54d;
+
+        // Impresion de numeros decimales
+        System.out.println("Numeros decimales: " + decimal1 + " , " + decimal2);
+
+
+
+        // Texto
+        String nombre = "Alejandro";
+        String apellido = "Fernandez";
+
+        // Caracter
+        char caracter1 = 'a';
+
+        // Impresion de texto
+        System.out.println("Texto: " + nombre + " , " + apellido + " , " + "Caracter: " + caracter1);
+
+
+
+        // Booleanos
+        boolean verdadero = true;
+        boolean falso = false;
+
+    }
+}
