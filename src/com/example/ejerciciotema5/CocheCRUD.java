d3edb90b5ff93974fb8da61352055b3775a944fc
alefernandezoficial <alefernandezoficial@gmail.com>
2023-01-24 21:48:37 -0300

    Updated

---
+++ src/com/example/ejerciciotema5/CocheCRUD.java
@@ -0,0 +1,13 @@
+package com.example.ejerciciotema5;
+
+    /**
+     * CocheCRUD
+     */
+
+     public interface CocheCRUD {
+
+        void save();
+        void findAll();
+        void delete();
+        
+    }
